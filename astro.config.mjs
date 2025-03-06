bf8406c055425b9e0ac44120bdb7bbe1838da5b5
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-03-05 22:30:12 -0500

    Add Astro environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +7,16 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  env: {
+    schema: {
+      // usando envField se puede definir el tipo de dato que se espera
+      // el contexto puede ser 'client' o 'server', en este caso astro trabaja en el servidor
+      // el acceso puede ser 'public' o 'private', por ejemplo, para api keys es recomensable 'private' porque astro nos informaria de cualquier vulnerabilidad
+      // en caso de que no se encuentre la variable de entorno, se usara el valor por defecto
+      SHOW_BUY_BUTTON: envField.boolean({ default: false, context: 'server', access: 'public' }),
+      // para leer una variable de entorno en el cliente se hace diferente a las de server
+      SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' }),
+    }
   }
 });
